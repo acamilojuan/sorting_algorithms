abe703dea89e9cf335f0a1876db4ad303a8ab285
David Hincapie <1508@holbertonschool.com>
2020-06-12 20:47:00 +0000

    Add bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ * bubble_sort - Sorting algorithm 
+ * @array: Array of integers
+ * 
+ * 
+ * 
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	int tmp;
+	int sorted = 1;
+
+	if(size >= 2)
+	{
+		for(; sorted == 1;)
+		{
+			sorted = 0;
+			for(i = 0; i < size - 1; i++)
+			{
+				if (array[i] > array[i + 1])
+				{
+					tmp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = tmp;
+					print_array(array, size);
+					sorted = 1;
+				}
+			}
+		}
+	}
+}
