370b4bf7d4029de45afcb4ea60a938b8bdfaa8e4
acamilojuan <1600@holbertonschool.com>
2020-06-15 23:29:32 +0000

    2-selection_sort.c betty fixed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,28 +1,28 @@
 #include "sort.h"
 /**
- * 
- * 
- * 
+ * selection_sort - Function to implement the Selection Sort Algorithm
+ * @array: Pointer to an arrey of integers
+ * @size: Variable of struct type
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j; 
+	size_t i, j;
 	int tmp, index, minor = 0;
 
-	if(size >= 2)
+	if (size >= 2)
 	{
-		for(i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			minor = array[i];
-			for(j = i + 1; j < size; j++)
+			for (j = i + 1; j < size; j++)
 			{
-				if(array[j] < minor)
+				if (array[j] < minor)
 				{
 					minor = array[j];
 					index = j;
 				}
 			}
-			if(minor != array[i])
+			if (minor != array[i])
 			{
 				tmp = array[i];
 				array[i] = minor;
